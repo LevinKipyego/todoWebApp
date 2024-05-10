60ffe65f798a0af2eb05ecfd6d43b9b269f0cea3
levi <levinkipyego@gmail.com>
2024-05-10 13:48:07 +0300

    first commit

---
+++ store.py
@@ -0,0 +1,19 @@
+instances=[
+    {'name':'jag',
+     'description':'blue',
+     'price':'$44',
+     'quality':'high'
+     },
+
+     {'name':'boot',
+     'description':'black',
+     'price':'$474',
+     'quality':'high'
+     },
+
+     {'name':'jag2',
+     'description':'blue',
+     'price':'$4',
+     'quality':'low'
+     }
+]
